6314786142dd43ca7c396bc2f3e39aebc453ec53
👨‍💻 Ramon Lucas Duarte | Future Software Engineer | Tech Enthusiast | Lifelong Learner <202407302246@alunos.estacio.br>
2025-03-21 21:53:56 +0000

    Implementando nível Aventureiro: menu interativo e comparação entre cartas

--- main.c
+++ main.c
@@ -1,61 +1,105 @@
 #include <stdio.h>
 
-// Estrutura que representa uma carta do Super Trunfo
-struct Carta {
-    char estado[50];
-    int codigo;
-    char nomeCidade[50];
-    int populacao;
-    float pib;
-    float area;
-    int pontosTuristicos;
-    float densidadePopulacional;
-    float pibPerCapita;
-};
+// Desafio Super Trunfo - Países
+// Tema 2 - Nível Aventureiro - Comparação com Menu Interativo
 
 int main() {
-    struct Carta carta;
+    // Definindo as variáveis para cada carta
+    int codigo1, populacao1, pontosTuristicos1;
+    char nome1[50];
+    float area1, pib1;
 
-    // Cadastro das informações básicas da cidade
-    printf("=== Cadastro de Carta Super Trunfo - Países ===\n");
+    int codigo2, populacao2, pontosTuristicos2;
+    char nome2[50];
+    float area2, pib2;
 
-    printf("Digite o estado: ");
-    fgets(carta.estado, sizeof(carta.estado), stdin);
+    int opcao;
 
+    // Cadastro da Carta 1
+    printf("=== Cadastro da Carta 1 ===\n");
     printf("Digite o código da cidade: ");
-    scanf("%d", &carta.codigo);
-    getchar(); // Limpa o buffer
-
+    scanf("%d", &codigo1);
+    getchar();
     printf("Digite o nome da cidade: ");
-    fgets(carta.nomeCidade, sizeof(carta.nomeCidade), stdin);
-
+    fgets(nome1, sizeof(nome1), stdin);
     printf("Digite a população: ");
-    scanf("%d", &carta.populacao);
-
-    printf("Digite o PIB (em bilhões): ");
-    scanf("%f", &carta.pib);
-
+    scanf("%d", &populacao1);
     printf("Digite a área (em km²): ");
-    scanf("%f", &carta.area);
-
+    scanf("%f", &area1);
+    printf("Digite o PIB (em bilhões de R$): ");
+    scanf("%f", &pib1);
     printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &carta.pontosTuristicos);
+    scanf("%d", &pontosTuristicos1);
 
-    // Cálculo de propriedades derivadas
-    carta.densidadePopulacional = carta.populacao / carta.area;
-    carta.pibPerCapita = (carta.pib * 1000000000) / carta.populacao;
+    getchar(); // Limpeza do buffer
 
-    // Exibição das informações
-    printf("\n=== Carta Registrada ===\n");
-    printf("Estado: %s", carta.estado);
-    printf("Código: %d\n", carta.codigo);
-    printf("Cidade: %s", carta.nomeCidade);
-    printf("População: %d\n", carta.populacao);
-    printf("PIB: %.2f bilhões\n", carta.pib);
-    printf("Área: %.2f km²\n", carta.area);
-    printf("Pontos Turísticos: %d\n", carta.pontosTuristicos);
-    printf("Densidade Populacional: %.2f hab/km²\n", carta.densidadePopulacional);
-    printf("PIB per capita: %.2f R$\n", carta.pibPerCapita);
+    // Cadastro da Carta 2
+    printf("\n=== Cadastro da Carta 2 ===\n");
+    printf("Digite o código da cidade: ");
+    scanf("%d", &codigo2);
+    getchar();
+    printf("Digite o nome da cidade: ");
+    fgets(nome2, sizeof(nome2), stdin);
+    printf("Digite a população: ");
+    scanf("%d", &populacao2);
+    printf("Digite a área (em km²): ");
+    scanf("%f", &area2);
+    printf("Digite o PIB (em bilhões de R$): ");
+    scanf("%f", &pib2);
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &pontosTuristicos2);
+
+    // Menu interativo
+    printf("\n=== Escolha o atributo para comparar ===\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("Digite a opção: ");
+    scanf("%d", &opcao);
+
+    // Comparação simples usando if-else
+    printf("\n=== Resultado ===\n");
+
+    if (opcao == 1) {
+        printf("Comparando População...\n");
+        if (populacao1 > populacao2)
+            printf("Vencedora: %s", nome1);
+        else if (populacao2 > populacao1)
+            printf("Vencedora: %s", nome2);
+        else
+            printf("Empate!\n");
+    }
+    else if (opcao == 2) {
+        printf("Comparando Área...\n");
+        if (area1 > area2)
+            printf("Vencedora: %s", nome1);
+        else if (area2 > area1)
+            printf("Vencedora: %s", nome2);
+        else
+            printf("Empate!\n");
+    }
+    else if (opcao == 3) {
+        printf("Comparando PIB...\n");
+        if (pib1 > pib2)
+            printf("Vencedora: %s", nome1);
+        else if (pib2 > pib1)
+            printf("Vencedora: %s", nome2);
+        else
+            printf("Empate!\n");
+    }
+    else if (opcao == 4) {
+        printf("Comparando Pontos Turísticos...\n");
+        if (pontosTuristicos1 > pontosTuristicos2)
+            printf("Vencedora: %s", nome1);
+        else if (pontosTuristicos2 > pontosTuristicos1)
+            printf("Vencedora: %s", nome2);
+        else
+            printf("Empate!\n");
+    }
+    else {
+        printf("Opção inválida.\n");
+    }
 
     return 0;
 }
